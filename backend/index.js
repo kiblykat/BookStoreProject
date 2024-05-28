59fc65bae296291eb9ef8087d21d90d0c6067a0f
izzat <izzatfadzlon@gmail.com>
2024-05-28 23:21:40 +0800

    added CORS package and implement

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,22 @@ import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import booksRoute from "./routes/booksRoute.js";
 
-//Setting up middleware (express running on
-//node, to access backend-i.e mongodb)
+//Middleware: express running on nodejs, to access backend-i.e mongodb
 const app = express();
-//IMPT!! Middleware for parsing request body
+//Middleware: for parsing request body
 app.use(express.json());
+
+//Middleware: for handling CORS policy
+//Option 1: Allow All
+app.use(cors({}));
+//Option 2: Custom Origin
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: [],
+//   })
+// );
 
 //any request to the /books endpoint will be router to booksRoute.js
 app.use("/books", booksRoute);
