40d5ac6396ee9c294e79bc03efa7470fff30e6a8
izzat <izzatfadzlon@gmail.com>
2024-05-29 23:40:50 +0800

    implement main FE, Home.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,11 +9,11 @@ import Home from "./pages/Home";
 const App = () => {
   return (
     <Routes>
-      <Route path="" element={Home} />
-      <Route path="" element={CreateBook} />
-      <Route path="" element={DeleteBook} />
-      <Route path="" element={ShowBook} />
-      <Route path="" element={EditBook} />
+      <Route path="/" element={<Home />} />
+      <Route path="/books/create" element={<CreateBook />} />
+      <Route path="/books/details/:id" element={<ShowBook />} />
+      <Route path="/books/edit/:id" element={<EditBook />} />
+      <Route path="/books/delete/:id" element={<DeleteBook />} />
     </Routes>
   );
 };
