71d25726fa994d2e1fc02813a6175643030d16ca
izzat <izzatfadzlon@gmail.com>
2024-05-29 08:21:13 +0800

    added react routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import CreateBook from "./pages/CreateBook";
+import DeleteBook from "./pages/DeleteBook";
+import ShowBook from "./pages/ShowBook";
+import EditBook from "./pages/EditBook";
+import Home from "./pages/Home";
 
 const App = () => {
-  return <div className="bg-red-400 text-white">App</div>;
+  return (
+    <Routes>
+      <Route path="" element={Home} />
+      <Route path="" element={CreateBook} />
+      <Route path="" element={DeleteBook} />
+      <Route path="" element={ShowBook} />
+      <Route path="" element={EditBook} />
+    </Routes>
+  );
 };
 
 export default App;
