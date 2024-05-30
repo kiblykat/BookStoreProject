70c42e032b5cbbc5283ac51711ee623916daca0a
izzat <izzatfadzlon@gmail.com>
2024-05-30 08:49:40 +0800

    showbook+backbtn 35m

--- frontend/src/pages/ShowBook.jsx
+++ frontend/src/pages/ShowBook.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import BackButton from "../components/BackButton";
+import Spinner from "../components/Spinner";
 
 const ShowBook = () => {
+  const [books, setBooks] = useState({});
+  const [loading, setLoading] = useState(false);
+  const { id } = useParams();
   return <div>ShowBook</div>;
 };
 
