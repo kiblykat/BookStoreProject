de5d4c666e648f9ffdd8798bb27c427d4cf90f52
izzat <izzatfadzlon@gmail.com>
2024-06-02 12:36:02 +0800

    separate booksCard and created dialogue box

---
+++ frontend/src/components/home/BookSingleCard.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { AiOutlineEdit } from "react-icons/ai";
+import { BiShow, BiUserCircle } from "react-icons/bi";
+import { BsInfoCircle } from "react-icons/bs";
+import { MdOutlineDelete } from "react-icons/md";
+import { PiBookOpenTextLight } from "react-icons/pi";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import BookModel from "./BookModel";
+
+const BookSingleCard = ({ book }) => {
+  const [showModel, setShowModel] = useState(false);
+  return (
+    <div
+      key={book._id}
+      className="border-2 border-gray-500 rounded-lg px-4 py-2 m-4 relative hover:shadow-xl"
+    >
+      <h2 className="absolute top-1 right-2 px-4 py-1 bg-red-400 rounded-lg">
+        {book.publishYear}
+      </h2>
+      <h4 className="my-2 text-gray-500">{book._id}</h4>
+      <div className="flex justify-start items-center gap-x-2">
+        <PiBookOpenTextLight className="text-red-300 text-2xl" />
+        <h2 className="my-1">{book.title}</h2>
+      </div>
+      <div className="flex justify-start items-center gap-x-2">
+        <BiUserCircle className="text-red-300 text-2xl" />
+        <h2 className="my-1">{book.author}</h2>
+      </div>
+      <div className="flex justify-between items-center gap-x-2 mt-4 p-4">
+        <BiShow
+          className="text-3xl text-blue-300 hover:text-black cursor-pointer"
+          onClick={() => setShowModel(true)}
+        />
+        <Link to={`/books/details/${book._id}`}>
+          <BsInfoCircle className="text-2xl text-green-800 hover:text-black" />
+        </Link>
+        <Link to={`/books/edit/${book._id}`}>
+          <AiOutlineEdit className="text-2xl text-yellow-600 hover:text-black " />
+        </Link>
+        <Link to={`/books/delete/${book._id}`}>
+          <MdOutlineDelete className="text-2xl text-red-600 hover:text-black" />
+        </Link>
+      </div>
+      {/* we are passing a setState function down into BookModel, to set showModel state to false. 
+      This state will be bubbled back up to parent(BookSingleCard) and cause the dialogue box to be closed */}
+      {showModel && (
+        <BookModel book={book} onClose={() => setShowModel(false)} />
+      )}
+    </div>
+  );
+};
+
+export default BookSingleCard;
